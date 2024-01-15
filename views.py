a9c20b22cd628c92af0833d6d775c0dc41d8df6f
Nathan <nathaniel.huber@stu.fortthomas.kyschools.us>
2024-01-15 15:10:31 -0500

    added files

---
+++ views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/info")
+def info():
+    return render_template("info.html")
